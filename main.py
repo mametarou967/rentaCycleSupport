c4ccf5da38a6e788fa1397961974277fa47c5401
mametarou967 <mametarou967@gmai.com>
2021-05-29 13:56:29 +0900

    weather add

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 import webbrowser ,sys
 import urllib.parse
+import requests
+import json
 # GPS
 import serial
 import micropyGPS
@@ -12,9 +14,14 @@ import RPi.GPIO as GPIO # RPi.GPIOモジュールを使用
 import csv
 import math
 
+API_KEY = "your api key"
+WEATHER_CONFIRM_INTERVAL = 60
+WEATHER_PIN = 16
+
 # GPS setup
 gpsLatitude = 0.0
 gpsLongitude = 0.0
+gpsValid = False
 rentaCyclePortList = []
 
 gps = micropyGPS.MicropyGPS(9, 'dd') # MicroGPSオブジェクトを生成する。
@@ -42,13 +49,36 @@ def event_callback(gpio_pin):
     print('current緯度経度: %2.8f, %2.8f' % (gpsLatitude, gpsLongitude))
     nearPointResult = nearPoint(gpsLatitude,gpsLongitude,rentaCyclePortList)
     print('near緯度経度: %2.8f, %2.8f' % (nearPointResult[0], nearPointResult[1]))
-    webbrowser.open("https://www.google.com/maps/dir/?api=1&origin=%2.8f,%2.8f&destination=%2.8f,%2.8f&travelmode=walking" % (gpsLatitude,gpsLongitude,nearPointResult[0], nearPointResult[1]),2,True)
+    if gpsValid == True:
+        webbrowser.open("https://www.google.com/maps/dir/?api=1&origin=%2.8f,%2.8f&destination=%2.8f,%2.8f&travelmode=walking" % (gpsLatitude,gpsLongitude,nearPointResult[0], nearPointResult[1]),2,True)
     # webbrowser.open("https://www.google.com/maps/dir/?api=1&origin=34.356722,132.337222&destination=34.4418632303169,132.41688967757966&travelmode=walking",2,True)
     
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.IN)
 GPIO.add_event_detect(18, GPIO.RISING, callback=event_callback, bouncetime=200)
+
+# Timer setup
+def helloWorld():
+    print("hello World")
+
+tim = threading.Timer(1,helloWorld)
+tim.start()
+
+def checkWeather(iLat,iLon):
+    url = "http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&APPID={key}"
+    url1 = url.format(lat = iLat,lon = iLon, key = API_KEY)
+    response = requests.get(url1)
+    data = response.json()
+    weather = data["weather"][0]["main"]
+
+    if((weather == "broken clouds") or (weather == "shower rain") or (weather == "rain") or (weather == "thunderstorm") or (weather == "snow") or (weather == "mist")
+       ):
+        print("weather bad")
+        return False
+    else:
+        print("weather good")
+        return True
 
 # Csv setup
 # 複数の座標のうちx, yに一番近い座標を求める
@@ -69,12 +99,22 @@ with open('portList.csv',encoding="utf_8") as file:
     for row in reader:
         rentaCyclePortList.append((float(row[0]),float(row[1])))
 
+badWeather = False
+start = time.time()
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(WEATHER_PIN,GPIO.OUT)
+GPIO.output(16,False)
 while True:
-    
     if gps.clean_sentences > 20: # ちゃんとしたデーターがある程度たまったら出力する
         h = gps.timestamp[0] if gps.timestamp[0] < 24 else gps.timestamp[0] - 24
         gpsLatitude = gps.latitude[0]
         gpsLongitude = gps.longitude[0]
-        # print('緯度経度: %2.8f, %2.8f' % (gps.latitude[0], gps.longitude[0]))
-        # print('')
+        if gpsLatitude > 0 and gpsLongitude > 0:
+            gpsValid = True
+    
+    elapsed_time = time.time() - start
+    if(badWeather == False and elapsed_time > WEATHER_CONFIRM_INTERVAL):
+        GPIO.output(16,True)
+        if(gpsValid == True and checkWeather(gpsLatitude,gpsLongitude)):
+            badWeather = True
     time.sleep(3.0)
